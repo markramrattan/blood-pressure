7b1cbc95c49f934a4c06db57721af3ca04cdd00e
markramrattan <mark.ramrattan.mbs@gmail.com>
2023-07-30 22:32:20 +0100

    Text Updates
    Guidance from doctor to change long text to bullet points

--- src/Info.js
+++ src/Info.js
@@ -3,20 +3,20 @@ import { Expander, ExpanderItem } from "@aws-amplify/ui-react";
 export default function Info() {
   return (
     <Expander type="single" isCollapsible={true}>
-    <ExpanderItem title="Systolic Pressure" value="bp-item-1">
-    The highest number is always the systolic pressure and it's always given first. The pressure when your heart pushes blood out around your body
-    </ExpanderItem>
-    <ExpanderItem title="Diastolic Pressure" value="bp-item-2">
-    The lower number is the diastolic pressure and it's always given second. The pressure when your heart rests between beats and blood is pushed around your heart
-    </ExpanderItem>
-    <ExpanderItem
-      title="General Guide"
-      value="bp-item-3"
-    >Normal blood pressure is considered to be between 90/60mmHg and 120/80mmHg (this applies if it's measured at home or at a GP surgery or clinic)
-  high blood pressure is considered to be 135/85 or higher if your reading was taken at home, or 140/90mmHg or higher if your reading was taken at a GP surgery or clinic
-  low blood pressure is considered to be lower than 89/59mmHg or lower
-    </ExpanderItem>
-  </Expander>
-      
+      <ExpanderItem title="Systolic Pressure" value="bp-item-1">
+        <li>Highest Number = Systolic pressure and it's always given first</li>
+        <li>The pressure when your heart pushes blood out around your body</li>
+      </ExpanderItem>
+      <ExpanderItem title="Diastolic Pressure" value="bp-item-2">
+        <li>Lowest Number = Diastolic pressure and it's always given second</li>
+        <li>The pressure when your heart rests between beats and blood is pushed around your heart</li>
+      </ExpanderItem>
+      <ExpanderItem title="General Guide" value="bp-item-3">
+        <li>Normal Blood Pressure (BP) = 120/80 - 90/60</li>
+        <li>High BP = 140/90 or above if done at GP Surgery</li>
+        <li>High BP = 135/85 or above if done at home</li>
+        <li>Low BP = 89/59 or below</li>
+      </ExpanderItem>
+    </Expander>
   );
 }
